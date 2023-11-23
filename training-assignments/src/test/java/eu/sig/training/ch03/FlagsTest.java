d3d7da5123a4cee995fade0cdaa7b40d9d69ea4f
Jos Klok <js_klok@hotmail.com>
2023-11-23 14:26:57 +0100

    comment added

--- training-assignments/src/test/java/eu/sig/training/ch03/FlagsTest.java
+++ training-assignments/src/test/java/eu/sig/training/ch03/FlagsTest.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 import org.junit.Test;
 
+/**
+ * 
+ */
 public class FlagsTest {
 
     @Test
--- training-assignments/src/test/java/eu/sig/training/ch04/AccountsTest.java
+++ training-assignments/src/test/java/eu/sig/training/ch04/AccountsTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Ignore;
 import org.junit.Test;
 
+/**
+ * 
+ */
 public class AccountsTest {
 
     @Test
