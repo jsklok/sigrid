0ebdc0f61a90c022bdaefb45b74caa8b8c55c1e0
Jos Klok <js_klok@hotmail.com>
2023-11-23 15:54:56 +0100

    sdfsdaf

--- src/java/main/eu/sig/training/maintainabilitybook/ch04_duplication/bankaccounts/CheckingAccount.java
+++ src/java/main/eu/sig/training/maintainabilitybook/ch04_duplication/bankaccounts/CheckingAccount.java
@@ -11,25 +11,19 @@ public class CheckingAccount {
 
     public Transfer makeTransfer(String counterAccount, Money amount)
         throws BusinessException {
+
         // 1. Check withdrawal limit:
         if (amount.greaterThan(this.transferLimit)) {
             throw new BusinessException("Limit exceeded!");
         }
+
         // 2. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0;
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
-            // 3. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount);
-            return result;
-        } else {
-            throw new BusinessException("Invalid account number!");
-        }
+        validateAccountNumber(counterAccount);        
+
+        // 3. Look up counter account and make transfer object:
+        CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+        Transfer result = new Transfer(this, acct, amount);
+        return result;
     }
 
     public void addInterest() {
@@ -40,5 +34,23 @@ public class CheckingAccount {
             balance.substract(interest);
         }
     }
+
+    /**
+     * 2. Assuming result is 9-digit bank account number, validate 11-test:
+     * @return 
+     * @throws BusinessException
+     */
+    public void validateAccountNumber(String counterAccount) throws BusinessException {
+        int sum = 0;
+        for (int i = 0; i < counterAccount.length(); i++) {
+            char character = counterAccount.charAt(i);
+            int characterValue = Character.getNumericValue(character);
+            sum = sum + (9 - i) * characterValue;
+        }
+        if(sum % 11 != 0 ) {
+            throw new BusinessException("Invalid account number!");
+        }
+    }
+
 }
 // end::CheckingAccount[]
